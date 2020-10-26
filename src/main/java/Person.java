c44340d584e643a637b07304fe4c517b2abad1bd
22rmisri <22rmisri@athenian.org>
2020-10-26 16:25:05 +0000

    person class

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,5 +1,39 @@
 public class Person {
-    
+    private String firstname;
+    private String lastname;
+   
+    public Person(String name_one, String name_two) {
+        firstname = name_one;
+        lastname = name_two;
+    }
+
+
+    public void doSomething() {
+        System.out.println("I am watching Netflix right now");
+    }
+
+    public String getfirstname() {
+        return this.firstname;
+    }
+    public String getlastname() {
+        return this.lastname;
+    }
+    public void setfirstname (String firstname) {
+        if (firstname.trim().length()!= 0) {
+            this.firstname = firstname;
+        }
+    } public void setlastname (String lastname) {
+        if (lastname.trim().length()!= 0) {
+            this.lastname = lastname;
+        }
+    }
+
+
+    public String toString() {
+        String str = firstname + " " + lastname;
+        return str;
+    }
+
 }
 
 
--- src/main/java/PersonClient.java
+++ src/main/java/PersonClient.java
@@ -1,5 +1,7 @@
 public class PersonClient {
     public static void main(String[] args) {
-        
-    }
-}
+        Person p1 = new Person("Isabella", "Smith");
+        p1.doSomething();
+        System.out.println(p1);
+        p1.setfirstname("Bella");
+        System.out.println(p1.getfirstname());
